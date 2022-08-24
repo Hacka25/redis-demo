5949d58fae396be459a787e089ba8078b22c9248
Paul Ambrose <pambrose@mac.com>
2022-08-24 16:22:18 -0700

    Update content

--- src/main/kotlin/Publish.kt
+++ src/main/kotlin/Publish.kt
@@ -6,16 +6,21 @@ object Publish {
   @JvmStatic
   fun main(args: Array<String>) {
     val jedis = JedisPooled("localhost", 6379)
+    val size = Stocks.values().size
 
     // Publish begin
     repeat(100) {
-      val stock = Stocks.values()[Random.nextInt(Stocks.values().size)].name
-      val channel = "stocks:$stock"
-      val temp = Random.nextInt(32, 100)
+      val stock = Stocks.values()[Random.nextInt(size)].name
+      val priceChannel = "stocks:price:$stock"
+      val volumeChannel = "stocks:volume:$stock"
+      val price = Random.nextInt(32, 100)
+      val volume = Random.nextInt(1000, 2000)
 
-      println("Publishing $channel price $temp")
+      println("Publishing $priceChannel price $price")
+      println("Publishing $volumeChannel volume $volume")
 
-      jedis.publish(channel, temp.toString())
+      jedis.publish(priceChannel, price.toString())
+      jedis.publish(volumeChannel, volume.toString())
 
       Thread.sleep(1000)
     }
--- src/main/kotlin/Subscribe.kt
+++ src/main/kotlin/Subscribe.kt
@@ -1,28 +1,36 @@
+import Stocks.AAPL
+import Stocks.GOOG
 import redis.clients.jedis.JedisPooled
 import redis.clients.jedis.JedisPubSub
+import java.util.concurrent.Executors
 
 
 object Subscribe {
   @JvmStatic
   fun main(args: Array<String>) {
     val jedis = JedisPooled("localhost", 6379)
+    val executor = Executors.newFixedThreadPool(2)
 
     // Subscribe begin
     class MyListener : JedisPubSub() {
       override fun onMessage(channel: String, message: String) {
-        println("Received a message for channel: $channel: $message")
+        println("Received a message for channel $channel: $message")
       }
 
-      override fun onSubscribe(channel: String, subscribedChannels: Int) {}
-      override fun onUnsubscribe(channel: String, subscribedChannels: Int) {}
-      override fun onPSubscribe(pattern: String, subscribedChannels: Int) {}
-      override fun onPUnsubscribe(pattern: String, subscribedChannels: Int) {}
+      override fun onSubscribe(channel: String, count: Int) {}
+      override fun onUnsubscribe(channel: String, count: Int) {}
+      override fun onPSubscribe(pattern: String, count: Int) {}
+      override fun onPUnsubscribe(pattern: String, count: Int) {}
       override fun onPMessage(pattern: String, channel: String, message: String) {}
     }
 
-    val myListener = MyListener()
+    executor.submit {
+      jedis.subscribe(MyListener(), "stocks:price:${AAPL.name}")
+    }
 
-    jedis.subscribe(myListener, "stocks:${Stocks.AAPL.name}")
+    executor.submit {
+      jedis.subscribe(MyListener(), "stocks:volume:${GOOG.name}")
+    }
 
     Thread.sleep(100000)
     // Subscribe end
